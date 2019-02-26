833aa11e3a7373896a51db5479b281d00fa1b3cd
Mann Khatri <khatri.mann@gmail.com>
2019-02-26 11:27:56 +0530

    Added all 4 assignments

---
+++ Assignment 4/conFusionServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://sdirectdb:sdirectdb@192.168.0.5:27017/sdirectdb',
+    'facebook': {
+        clientId: 'Your App ID',
+        clientSecret: 'Your App Secret'
+    }
+}
